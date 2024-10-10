b7b49f0b3ff87b37c26a0c5d27b2006c0fa0b2fd
Bruno Figueroa Rodriguez <brunofigui@gmail.com>
2024-10-10 17:17:52 +0200

    WIP Loop

---
+++ src/main/java/com/campusdual/loops/WhileLoop.java
@@ -0,0 +1,20 @@
+package com.campusdual.loops;
+
+import com.campusdual.Utils;
+
+public class WhileLoop {
+
+   public static void verifyNumbers(){
+       int num = Utils.integer("Write a number greater than 10 and lower than 20: ");
+       while(num <= 10 || num >= 20){
+           num = Utils.integer("not a valid number, try again");
+       }
+
+       System.out.println("Number " + num + "is in range");
+
+   }
+
+    public static void main(String [] args){
+        verifyNumbers();
+    }
+}
