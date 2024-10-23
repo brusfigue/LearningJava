a6f122ec030564e3c76e941823bfd0691089de48
Bruno Figueroa Rodriguez <brunofigui@gmail.com>
2024-10-23 09:50:23 +0200

    interfaces

---
+++ src/main/java/com/campusdual/interfaces/IBooking.java
@@ -0,0 +1,11 @@
+package com.campusdual.interfaces;
+
+import java.util.Date;
+
+public interface IBooking {
+    void book (Date bookDate, String... args);
+    void cancelBook();
+    void getTicketDetail();
+
+
+}
